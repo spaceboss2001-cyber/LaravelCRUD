26e46973daca82d945161b3a4b114445081b48ac
spaceboss2001-cyber <spaceboss2001@gmail.com>
2025-11-29 16:42:06 +0100

    Update web.php

--- web.php
+++ web.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\DailymenuController;
+use App\Http\Controllers\CarpartsController;
 use App\Http\Controllers\ReservationController;
-use App\Http\Controllers\DishesController;
+use App\Http\Controllers\CarsController;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
@@ -10,27 +10,27 @@ use App\Livewire\Settings\TwoFactor;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
-Route::get('/', [DishesController::class, 'index'])->name('home');
+Route::get('/', [CarsController::class, 'index'])->name('home');
 
 Route::middleware(['auth', 'role:owner,admin,cook'])->group(function () {
 
-    Route::get('dishes', [DishesController::class, 'index'])->name('dishes');
-    Route::get('edit', [DishesController::class, 'index'])->name('edit');
-    Route::get('/gerechten', [DishesController::class, 'viewDishes'])->name('gerechten');
-    Route::delete('/dishes/{dish}', [DishesController::class, 'destroy'])->name('dishes.destroy');
-    Route::post('/dishes', [DishesController::class, 'store'])->name('dishes.store');
-    Route::get('/dishes/{dish}/edit', [DishesController::class, 'edit'])->name('dishes.edit');
-    Route::put('/dishes/{dish}', [DishesController::class, 'update'])->name('dishes.update');
+    Route::get('cars', [CarsController::class, 'index'])->name('cars');
+    Route::get('edit', [CarsController::class, 'index'])->name('edit');
+    Route::get('/cars/view', [CarsController::class, 'viewCars'])->name('cars.view');
 
-    Route::get('dailymenus', [DailymenuController::class, 'index'])->name('dailymenus.index');
-    Route::post('dailymenus', [DailymenuController::class, 'store'])->name('dailymenus.store');
-    Route::get('/dailymenus/show', [DailymenuController::class, 'show'])->name('dailymenus.show');
-    Route::delete('/dailymenus/{dailymenus}', [DailymenuController::class, 'destroy'])->name('dailymenus.destroy');
+    Route::delete('/cars/{car}', [CarsController::class, 'destroy'])->name('cars.destroy');
+    Route::post('/cars', [CarsController::class, 'store'])->name('cars.store');
+    Route::get('/cars/{car}/edit', [CarsController::class, 'edit'])->name('cars.edit');
+    Route::put('/cars/{car}', [CarsController::class, 'update'])->name('cars.update');
+
+    Route::get('carparts', [CarpartsController::class, 'index'])->name('carparts.index');
+    Route::post('carparts', [CarpartsController::class, 'store'])->name('carparts.store');
+    Route::get('/carparts/show', [CarpartsController::class, 'show'])->name('carparts.show');
+    Route::delete('/carparts/{carparts}', [CarpartsController::class, 'destroy'])->name('carparts.destroy');
 });
 
 Route::middleware(['auth'])->group(function () {
-
-Route::get('reservation', [ReservationController::class, 'index'])->name('reservation');
+    Route::get('reservation', [ReservationController::class, 'index'])->name('reservation');
 });
 
 Route::middleware(['auth', 'role:owner,admin'])->group(function () {
