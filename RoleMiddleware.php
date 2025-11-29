645af1d4c299c2ad1eddd4684f816a58f65537ec
spaceboss2001-cyber <spaceboss2001@gmail.com>
2025-11-29 16:46:30 +0100

    Add files via upload

---
+++ RoleMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, ...$roles): Response
+    {
+        $user = $request->user();
+
+        if (!$user) {
+            return redirect('/');
+        }
+
+        if (!in_array($user->roles, $roles)) {
+            return redirect('/');
+        }
+
+        return $next($request);
+
+    }
+}
